9620837bcdad77d9cf360ffbf30789119d1ddaf6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-01 11:56:25 +0000

    config: Add PERF_ANIM_OVERRIDE flag
    Change-Id: I7d264207abef968739b57c9fce19e8f65466ee8c

--- config/common.mk
+++ config/common.mk
@@ -49,6 +49,12 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     is_expressive_design_enabled=true
     
+# Animation override
+PERF_ANIM_OVERRIDE ?= false
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.activity_anim_perf_override=$(PERF_ANIM_OVERRIDE)
+
 # Bypass charging
 BYPASS_CHARGE_SUPPORTED ?= false
 PRODUCT_SYSTEM_PROPERTIES += \
